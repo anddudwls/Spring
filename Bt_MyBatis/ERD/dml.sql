27042102ca322299407a9a4dfbdc1926633ecd1c
anddudwls <83632576+anddudwls@users.noreply.github.com>
2021-12-07 12:40:31 +0900

    Add files via upload

---
+++ Bt_MyBatis/ERD/dml.sql
@@ -0,0 +1,13 @@
+SELECT 
+	wr_uid uid, 
+	wr_subject subject, 
+	wr_content content, 
+	wr_name name, 
+	wr_viewcnt viewcnt, 
+	wr_regdate regDate 
+FROM 
+	test_write 
+ORDER BY 
+	wr_uid DESC	;
+			
+delete from test_write where wr_uid > 10;		
