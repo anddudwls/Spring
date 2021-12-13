58649a1efe82d456f9741e5a6c34cde56be8a9be
anddudwls <83632576+anddudwls@users.noreply.github.com>
2021-12-13 19:06:41 +0900

    Add files via upload

---
+++ Bt_JPA/src/test/java/com/lec/spring/controller/HelloWorldControllerTest.java
@@ -0,0 +1,19 @@
+package com.lec.spring.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorldControllerTest {
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	void test2() {
+		System.out.println("Hello JUnit");
+	}
+
+}
