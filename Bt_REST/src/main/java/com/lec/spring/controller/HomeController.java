57176f2e29b66fd4504f7bfc5611457c8959d412
anddudwls <83632576+anddudwls@users.noreply.github.com>
2021-12-14 18:08:45 +0900

    Add files via upload

---
+++ Bt_REST/src/main/java/com/lec/spring/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.lec.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/home")
+	@ResponseBody
+	public String home() {
+		// view 를 response
+		
+		return "hello my home sweet home!";
+	}
+	
+
+	
+}
+
+
+
+
+
